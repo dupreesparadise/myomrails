154fca5ff0eaeda51b37bde927f70ac8ac60235d
Edward Roberts <dupreesparadise@gmail.com>
2013-08-06 13:21:16 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myomrails::Application.config.secret_key_base = 'b934bc3632f412aca8e7164b615fbd252686bc428f2d7d934bc31b3f58390847bd6dcbaac2f15d37e925c0bb6c5c5cad72666ba2267e0cf95877a690ddd0caa8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Myomrails::Application.config.session_store :cookie_store, key: '_myomrails_session'
